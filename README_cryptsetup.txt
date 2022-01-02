624d4c7a4ad4a2e1d35e8687a789a8f7d2e5deac
Daniel Zatovic <daniel.zatovic@gmail.com>
2022-01-03 00:38:23 +0100

    Add cryptsetup project

--- README_cryptsetup.txt
+++ README_cryptsetup.txt
@@ -1,7 +1,14 @@
 sudo python infra/helper.py build_image cryptsetup
 sudo python infra/helper.py build_fuzzers cryptsetup
 
-# on selinux systems:
+# On selinux systems:
 sudo chcon -Rt svirt_sandbox_file_t build/
 
-sudo python infra/helper.py run_fuzzer cryptsetup crypt_load_fuzz
+# Run LUKS2 fuzzer
+sudo python infra/helper.py run_fuzzer --corpus-dir build/corpus/cryptsetup/crypt2_load_fuzz/ --sanitizer address cryptsetup crypt2_load_fuzz -jobs=8 -workers=8
+
+# Rebuild fuzz targets for coverage
+sudo python infra/helper.py build_fuzzers --sanitizer coverage cryptsetup
+
+# Generate coverage report
+sudo python infra/helper.py coverage cryptsetup --no-corpus-download --fuzz-target crypt2_load_fuzz
--- projects/cryptsetup/build.sh
+++ projects/cryptsetup/build.sh
@@ -1,43 +1,57 @@
-#!/bin/bash -eu
-# Copyright 2021 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-################################################################################
+#!/usr/bin/env bash
 
+set -ex
 
-cd ../lvm2
+export LC_CTYPE=C.UTF-8
 
+export CC=${CC:-clang}
+export CXX=${CXX:-clang++}
+export LIB_FUZZING_ENGINE=${LIB_FUZZING_ENGINE:--fsanitize=fuzzer}
+
+SANITIZER=${SANITIZER:-address -fsanitize-address-use-after-scope}
+flags="-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=$SANITIZER -fsanitize=fuzzer-no-link"
+
+export CFLAGS=${CFLAGS:-$flags}
+export CXXFLAGS=${CXXFLAGS:-$flags}
+
+echo "lvm2 lvm2 lvm2 lvm2 lvm2 lvm2 lvm2 lvm2 lvm2 lvm2 "
+cd lvm2
 ./configure --enable-static_link --disable-selinux
 # make -j$(nproc) ./device_mapper/libdevice-mapper.a
 # make -j$(nproc) libdm
-make -j$(nproc) libdm.device-mapper
+make V=1 -j$(nproc) libdm.device-mapper
+cd ..
 
-cd ../cryptsetup
+echo "libuuid libuuid libuuid libuuid libuuid libuuid libuuid libuuid libuuid libuuid "
+cd util-linux-2.37.2/
+./configure --enable-static
+make V=1 -j libuuid.la
+cd ..
 
-# build project
-# e.g.
+echo "jsonc jsonc jsonc jsonc jsonc jsonc jsonc jsonc jsonc jsonc "
+cd json-c-0.15
+mkdir build
+cd build
+cmake -DBUILD_STATIC_LIBS=ON ..
+make VERBOSE=1 -j json-c-static
+cd ../..
+
+echo "popt popt popt popt popt popt popt popt popt popt "
+cd popt-1.18/
+./configure --enable-static
+make V=1 -j
+cd ..
+
+echo "openssl openssl openssl openssl openssl openssl openssl openssl openssl openssl "
+cd openssl-1.1.1m/
+./config
+make V=1 -j
+cd ..
+
+echo "csetup csetup csetup csetup csetup csetup csetup csetup csetup csetup "
 ./autogen.sh
-./configure --enable-static --disable-ssh-token
-make -j$(nproc) all
-
-# ../lvm2/device_mapper/libdevice-mapper.a \
-
-# build fuzzers
-# e.g.
-# $CXX $CXXFLAGS -std=c++11 -Iinclude \
-#     /path/to/name_of_fuzzer.cc -o $OUT/name_of_fuzzer \
-#     $LIB_FUZZING_ENGINE /path/to/library.a
+./configure --enable-static --disable-ssh-token --disable-blkid --disable-udev --disable-selinux --disable-pwquality --with-crypto_backend=openssl --disable-cryptsetup --disable-veritysetup --disable-integritysetup
+make V=1 -j$(nproc) all
 
 for fuzzer in tests/fuzz/*_fuzz.cc
 do
@@ -46,31 +60,18 @@ do
      $fuzzer -o $OUT/$fuzzer_name \
      $LIB_FUZZING_ENGINE \
      .libs/libcryptsetup.a \
-     ../lvm2/libdm/ioctl/libdevmapper.a \
+     lvm2/libdm/ioctl/libdevmapper.a \
+     util-linux-2.37.2/.libs/libuuid.a \
+     json-c-0.15/build/libjson-c.a \
+     popt-1.18/src/.libs/libpopt.a \
+     openssl-1.1.1m/libcrypto.a \
+     openssl-1.1.1m/libssl.a \
 	-lm \
-     	-Wl,-Bstatic \
-	-luuid \
-	-lssl \
-	-lcrypto \
-	-ljson-c \
-	-lblkid \
-	-lpopt \
-	-luuid \
-	-lblkid \
-	-lm \
-	-lpopt \
-	-lblkid \
-	-lm \
-	-lpopt \
-	-luuid \
-	-lblkid \
-	-lm \
-	-lpopt \
-	-luuid \
-	-lblkid \
-	-Wl,-Bdynamic \
 	-Wl,-R$OUT
   cp tests/fuzz/${fuzzer_name}_seed_corpus.zip $OUT
 done
 cp .libs/libcryptsetup.so $OUT/
+#cp -r $SRC ${OUT}/src
+cp $SRC/cryptsetup/*.dict $OUT
+
 #ln -s $OUT/libcryptsetup.so $OUT/libcryptsetup.so.12
